821404f62e0fdf4f3c242fd7a35aba4546d1cff8
Brennan Gebotys <gebob19.git@gmail.com>
2019-05-19 12:57:11 -0400

    lazy update

--- train.py
+++ train.py
@@ -206,12 +206,14 @@ def train(args):
                 loss_m.append(round(loss.item() / len(targets), 4))
                 n_correct, n_examples = accuracy(preds, targets)
                 accuracy_m.append((n_correct.float() / n_examples).item())
-
+                torch.cuda.empty_cache()
+                
                 print(('epoch %d, train itr %d, avg. loss %.2f, '
                         'train accuracy: %.2f, '
                         'time elapsed %.2f sec') % (e, train_iter,
                         epoch_loss / train_iter, accuracy_m[-1],
                         time.time() - begin_time), file=sys.stderr)
+
 
             if args['--qtest'] and train_iter > 5: break
 
