890b81e75674705d6aeb513145406ad3e054c50c
Brennan Gebotys <gebob19.git@gmail.com>
2019-05-23 23:34:02 -0400

    lazy update

--- init.py
+++ init.py
@@ -16,37 +16,48 @@ def extract_helper(path, target):
     return exs, labels, ratings
 
 if __name__ == '__main__':
-    print('Extracting Data...')
-    # parse + structure the *train* data
-    neg_exs, neg_labels, neg_ratings = extract_helper('train/neg/', 0)
-    pos_exs, pos_labels, pos_ratings = extract_helper('train/pos/', 1)
-    
-    df = pd.DataFrame(data={'path': neg_exs + pos_exs,
-                            'target': neg_labels + pos_labels,
-                            'review_rating': neg_ratings + pos_ratings})
-    df = df.sample(frac=1.)
-    df.to_csv('train.csv', index=False)
+    # quick and dirty update - update the length of the docs
+    if True:
+        train_df = pd.read_csv('train.csv')
+        lengths = []
+        for path in train_df['path'].values:
+            file = open(str(base/path), encoding='utf-8').read()
+            lengths.append(len(file.split(' ')))
+        train_df['file_length'] = lengths
+        train_df.to_csv('train.csv', index=False)
 
-    # parse + structure the *test* data
-    neg_exs, neg_labels, neg_ratings = extract_helper('test/neg/', 0)
-    pos_exs, pos_labels, pos_ratings = extract_helper('test/pos/', 1)
-
-    test_df = pd.DataFrame(data={'path': neg_exs + pos_exs,
+    else:
+        print('Extracting Data...')
+        # parse + structure the *train* data
+        neg_exs, neg_labels, neg_ratings = extract_helper('train/neg/', 0)
+        pos_exs, pos_labels, pos_ratings = extract_helper('train/pos/', 1)
+        
+        df = pd.DataFrame(data={'path': neg_exs + pos_exs,
                                 'target': neg_labels + pos_labels,
                                 'review_rating': neg_ratings + pos_ratings})
-    test_df = test_df.sample(frac=1.)
-    test_df.to_csv('test.csv', index=False)
+        df = df.sample(frac=1.)
+        df.to_csv('train.csv', index=False)
 
-    print('Reading Corpus...')
-    # read the corpus into a language data model
-    lang = Lang()
-    populate_language(lang, df, base)
-    dump_model(lang)
+        # parse + structure the *test* data
+        neg_exs, neg_labels, neg_ratings = extract_helper('test/neg/', 0)
+        pos_exs, pos_labels, pos_ratings = extract_helper('test/pos/', 1)
 
-    # make dir for saves
-    os.mkdir('model_saves')
-    os.mkdir('metric_saves')
-    
+        test_df = pd.DataFrame(data={'path': neg_exs + pos_exs,
+                                    'target': neg_labels + pos_labels,
+                                    'review_rating': neg_ratings + pos_ratings})
+        test_df = test_df.sample(frac=1.)
+        test_df.to_csv('test.csv', index=False)
+
+        print('Reading Corpus...')
+        # read the corpus into a language data model
+        lang = Lang()
+        populate_language(lang, df, base)
+        dump_model(lang)
+
+        # make dir for saves
+        os.mkdir('model_saves')
+        os.mkdir('metric_saves')
+        
     print('FIN')
-    
+        
 
