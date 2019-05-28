9a2e0df0a5a7eebb7d61e380020f3f9cab1a907e
Brennan Gebotys <gebob19.git@gmail.com>
2019-05-28 15:42:22 -0400

    implm RNN attention model

--- model.py
+++ model.py
@@ -4,15 +4,83 @@ import torch.nn.functional as F
 
 from utils import to_input_tensor
 
+class SaveModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def save(self, path):
+        params = {
+            'vocab': self.language,
+            'state_dict': self.state_dict()
+        }
+        torch.save(params, path)
+
+class RNN_Self_Attention_Classifier(SaveModel):
+    def __init__(self, language, device, batch_size, embed_dim, hidden_dim, num_embed, n_classes):
+        super().__init__()
+        self.device = device
+        self.language = language 
+        self.batch_size = batch_size
+        
+        self.embedding = nn.Embedding(num_embed, embed_dim)
+        self.gru = nn.GRU(embed_dim, hidden_dim, bias=True)
+        self.classify = nn.Linear(2 * hidden_dim, n_classes)
+        self.attention = RNNAttention()
+        
+    def forward(self, sents):
+        # Embed the sequence
+        x, lengths = to_input_tensor(self.language, sents, self.device)
+        x_embed = self.embedding(x)
+        # RNN encoding
+        x = nn.utils.rnn.pack_padded_sequence(x_embed, lengths)
+        x, _ = self.gru(x)
+        x, _ = nn.utils.rnn.pad_packed_sequence(x)
+        
+        x = x.transpose(0, 1)
+        # get attention over RNN outputs 
+        I = torch.eye(max(lengths))
+        attn_mask = torch.stack([I] * self.batch_size)
+        for i, l in zip(list(range(self.batch_size)), lengths):
+            attn_mask[i, :, l:] = 1
+            attn_mask[i, l:, :] = 1
+        
+        attn = self.attention(x, attn_mask)
+        attn_vec = attn.unsqueeze(-1) * x.unsqueeze(1)
+        attn_vec = attn_vec.sum(-2)
+        attn_out = torch.cat([attn_vec, x], dim=-1)
+        
+        # max pool over sequence 
+        attn_out = attn_out.transpose(-1, -2)
+        max_vec, _ = torch.max(attn_out, -1)
+        max_vec = max_vec.unsqueeze(-2)
+        
+        # binary classification activ.
+        y = torch.sigmoid(self.classify(max_vec)).squeeze()
+        return y        
+    
+class RNNAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x, attn_mask):
+        # apply attention over RNN outputs (batch, seq, hidden)
+        attn = torch.bmm(x, x.transpose(1, 2))
+        attn.data.masked_fill_(attn_mask.byte(), -float('inf'))
+        attn = torch.softmax(attn, dim=2)
+        # account for padding 
+        attn.data.masked_fill_(attn_mask.byte(), 0)
+        return attn   
+
+
 # Thanks to, 
 # https://twitter.com/Thom_Wolf/status/1129658539142766592
 # AND using the Transformer as Classifier Thanks To, 
 # https://www.aclweb.org/anthology/W18-5429
-class TransformerClassifier(nn.Module):
-    def __init__(self, lang, device, embed_dim, hidden_dim, num_embed, num_pos, num_heads, num_layers, dropout, n_classes):
+class TransformerClassifier(SaveModel):
+    def __init__(self, language, device, embed_dim, hidden_dim, num_embed, num_pos, num_heads, num_layers, dropout, n_classes):
         super().__init__()
         self.device = device
-        self.lang = lang
+        self.language = language
         
         self.encoder = TransformerEmbedder(embed_dim, num_embed, num_pos, dropout)
         self.dropout = nn.Dropout(dropout)
@@ -58,12 +126,6 @@ class TransformerClassifier(nn.Module):
         y = torch.sigmoid(self.classify(x)).squeeze(-1)
         return y
 
-    def save(self, path):
-        params = {
-            'vocab': self.lang,
-            'state_dict': self.state_dict()
-        }
-        torch.save(params, path)
 
 # seperate module to make attention analysis easy
 class TransformerEmbedder(nn.Module):
