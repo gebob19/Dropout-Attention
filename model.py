8db8df18be527299618d554fff138e869d6b49f0
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-03 15:21:18 -0400

    lazy update

--- model.py
+++ model.py
@@ -33,6 +33,7 @@ class TaskSpecificAttention(SaveModel):
         self.tasks = []
         self.attention = TaskAttention()
         self.maxpool = nn.MaxPool1d(8)
+        self.ln3 = nn.LayerNorm(hidden_dim, eps=1e-12)
         
         for i in range(num_layers):
             self.mhas.append(nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout))
@@ -48,12 +49,12 @@ class TaskSpecificAttention(SaveModel):
             self.ln_1.append(nn.LayerNorm(embed_dim, eps=1e-12))
             self.ln_2.append(nn.LayerNorm(embed_dim, eps=1e-12))
         
-        self.classify = nn.Linear(embed_dim, n_classes)
+        # self.classify = nn.Linear(embed_dim, n_classes)
 
         self.h1 = nn.Linear(self.final_dim, hidden_dim)
         self.h2 = nn.Linear(hidden_dim, hidden_dim)
         self.h3 = nn.Linear(hidden_dim, 1)
-        # self.classify = nn.Linear(hidden_dim, n_classes)
+        self.classify = nn.Linear(hidden_dim, n_classes)
         
     def forward(self, sents):
         batch_size = len(sents)
@@ -89,21 +90,22 @@ class TaskSpecificAttention(SaveModel):
         # bs, embed, seq
         x = x.transpose(1, 2)
 
-        # pool + padding
-        p = self.maxpool(x)
-        diff = self.final_dim - p.size(-1)
-        pad = torch.zeros((p.size(0), p.size(1), diff), device=self.device)
-        pad.require_grad = False
-        x = torch.cat([p, pad], -1)
+        # # pool + padding
+        # p = self.maxpool(x)
+        # diff = self.final_dim - p.size(-1)
+        # pad = torch.zeros((p.size(0), p.size(1), diff), device=self.device)
+        # pad.require_grad = False
+        # x = torch.cat([p, pad], -1)
 
-        # classification layers
-        x = F.relu(self.h1(x))
-        x = F.relu(self.h2(x))
-        x = self.h3(x.transpose(-1, -2)).squeeze()
-        y = F.tanh(self.classify(x)).squeeze()
+        # # classification layers
+        # x = F.relu(self.h1(x))
+        # x = F.relu(self.h2(x))
+        # x = self.h3(x.transpose(-1, -2)).squeeze()
+        # y = torch.sigmoid(self.classify(x)).squeeze()
 
-        # m, _ = torch.max(x, -1)
-        # y = F.tanh(self.classify(m)).squeeze()
+        m, _ = torch.max(x, -1)
+        m = self.ln3(m)
+        y = torch.sigmoid(self.classify(m)).squeeze()
         
         return y
 
