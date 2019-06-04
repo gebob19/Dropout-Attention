08f667e4740ac663f07dbe1bf986af384a76a98c
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-04 11:06:38 -0400

    lazy update

--- language_structure.py
+++ language_structure.py
@@ -51,10 +51,10 @@ class Lang:
             top_lang.word2count[w] = f 
             
             # retain id to word for consistent embeddings
-            id = self.word2id[w]
-            top_lang.word2id[w] = id
-            top_lang.id2word[id] = w
-            
+            # id = self.word2id[w]
+            # top_lang.word2id[w] = id
+            # top_lang.id2word[id] = w
+
         return top_lang
     
     def get_id(self, word):
