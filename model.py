53ebca8092bdb4051411d263edec8d126bbd3e3a
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-05 17:50:33 -0400

    lazy update

--- model.py
+++ model.py
@@ -107,7 +107,8 @@ class TaskSpecificAttention(SaveModel):
             # x = self.weight1 * self.attention(x, te)
             # x = self.attention(w_embed, te) + self.attention(x, te)
             # x = x + self.weight1 * self.attention(w_embed, te) * w_embed
-            h = x + h * self.attention(h, te)
+            # h = x + h * self.attention(h, te)
+            h = x + w_embed * self.attention(w_embed, te)
             # h = x + h
             h = lnorm_1(h)
             
@@ -121,7 +122,9 @@ class TaskSpecificAttention(SaveModel):
             # x = self.weight2 * self.attention(x, ffe)
             # x = self.attention(w_embed, ffe) + self.attention(x, ffe) * x
             # x = x + self.weight2 * self.attention(w_embed, ffe) * w_embed
-            h = x + h * self.attention(h, ffe)
+            # h = x + h * self.attention(h, ffe)
+            h = x + w_embed * self.attention(w_embed, ffe)
+            # h = x + h 
             h = lnorm_2(h)
 
         # bs, seq, embed_dim
