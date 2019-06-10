d8d9398c99bd443f283a0718a6ff9be1e8980953
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-09 22:05:07 -0400

    lazy update

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import torch
 
+import matplotlib.pyplot as plt
+
 from language_structure import normalizeString
 
 def to_input_tensor(lang, sents, device):
@@ -42,3 +44,93 @@ def clip_sents(sents, max_sentence_len):
         return s
     sents_clipped = [clip(s) for s in sents]
     return sents_clipped
+
+
+## Vis. Helpers 
+
+    formatted_sentence.append(str(len(idxs)+1))
+    
+    return ' '.join(formatted_sentence)
+
+def plot_metrics(metrics, first_n=100):
+    print("Total Time: {} \nTrain Time: {}".format(metrics['total_time'], metrics['train_time']))
+    print("Max (Validation, Train): (%.2f, %.2f)" % (max(metrics['val_acc']), max(metrics['train_acc'])))
+    
+    metrics['train_loss'] = metrics['train_loss'][:first_n]
+    metrics['val_loss'] = metrics['val_loss'][:first_n]
+    metrics['train_acc'] = metrics['train_acc'][:first_n]
+    metrics['val_acc'] = metrics['val_acc'][:first_n]
+        
+    metrics['itr'] = list(range(len(metrics['train_loss'])))
+    metrics['val_itr'] = list(range(len(metrics['val_loss'])))
+    
+    figsize = (20, 15)
+    plt.figure(1, figsize=figsize)                
+    plt.subplot(211)
+    plt.plot(metrics['itr'], metrics['train_loss'],  label='train')
+    plt.plot(metrics['val_itr'], metrics['val_loss'], label='validation')
+    plt.legend()
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Loss')
+    plt.title('Model Loss')
+    plt.show()
+    
+    # plot accuracy
+    plt.figure(2, figsize=figsize)                
+    plt.subplot(212)
+    plt.plot(metrics['itr'], metrics['train_acc'], label='train')
+    plt.plot(metrics['val_itr'], metrics['val_acc'], label='validation')
+    plt.legend()
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Accuracy')
+    plt.title('Model Accuracy @ 50% Threshold')
+    
+    plt.show()
+
+
+
+def compare_metrics(metrics, first_n=100):
+    # metric pre-processing
+    for m in metrics:
+        for key in ['val_acc', 'train_acc', 'val_loss', 'train_loss']:
+            m[key] = m[key][:first_n]
+        m['itr'] = list(range(len(m['train_loss'])))
+        m['val_itr'] = list(range(len(m['val_loss'])))
+    names = [m['args']['--save-to'] for m in metrics]    
+
+    for m in metrics:
+        print('--------{}----------'.format(m['args']['--save-to']))
+        print("Max (Validation, Train): (%.2f, %.2f)" % (max(m['val_acc']), max(m['train_acc'])))
+    
+    figsize = (15, 15)
+    
+    plt.figure(1, figsize=figsize)                
+    plt.subplot(211)
+    for m, name in zip(metrics, names):
+        plt.plot(m['itr'], m['train_loss'],  label='{}-train'.format(name))
+        plt.plot(m['val_itr'], m['val_loss'], label='{}-validation'.format(name))
+    plt.legend()
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Loss')
+    plt.title('Model Loss')
+    plt.show()
+    
+    # plot accuracy
+    plt.figure(2, figsize=figsize)                
+    plt.subplot(212)
+    for m, name in zip(metrics, names):
+        plt.plot(m['itr'], m['train_acc'],  label='{}'.format(name))
+    plt.legend()
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Accuracy')
+    plt.title('Model Training Accuracy @ 50% Threshold')
+    
+    # plot accuracy
+    plt.figure(3, figsize=figsize)                
+    plt.subplot(211)
+    for m, name in zip(metrics, names):
+        plt.plot(m['val_itr'], m['val_acc'], label='{}'.format(name))
+    plt.legend()
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Accuracy')
+    plt.title('Model Validation Accuracy @ 50% Threshold')
