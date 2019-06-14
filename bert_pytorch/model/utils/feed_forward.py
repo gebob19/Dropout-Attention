a13b265cf7e84390ec0c0f2ba9a33c2038c3f52d
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-14 10:28:16 -0400

    lazy update

--- bert_pytorch/model/utils/feed_forward.py
+++ bert_pytorch/model/utils/feed_forward.py
@@ -7,10 +7,15 @@ class PositionwiseFeedForward(nn.Module):
 
     def __init__(self, d_model, d_ff, dropout=0.1):
         super(PositionwiseFeedForward, self).__init__()
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
-        self.activation = GELU()
+        # self.w_1 = nn.Linear(d_model, d_ff)
+        # self.w_2 = nn.Linear(d_ff, d_model)
+        # self.dropout = nn.Dropout(dropout)
+        # self.activation = GELU()
+
+        self.ff = nn.Sequential(nn.Linear(d_model, d_ff),
+                                    nn.ReLU(), 
+                                    nn.Linear(d_ff, d_model))
 
     def forward(self, x):
-        return self.w_2(self.dropout(self.activation(self.w_1(x))))
+        return self.ff(x)
+        # return self.w_2(self.dropout(self.activation(self.w_1(x))))
