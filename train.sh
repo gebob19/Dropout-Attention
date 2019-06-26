4437a85fa66ac3cf2c97c3e7c87408258ef8f545
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-26 17:08:49 -0400

    lazy commit

--- train.sh
+++ train.sh
@@ -1,48 +1,13 @@
 # VAR SETUP
 ## Model Arch.
 hidden_size="384"
-n_heads="12"
-n_layers="12"
+n_heads="6"
+n_layers="6"
 ## Training Params
 batch_size="16"
 epochs="2"
-id="JUNE25-10K-12LAYER"
+id="JUNE26-UPDATED-6LAYER"
 dset_size="10000"
-
-# # RTE
-# ## Vanilla Dropout 
-# python train.py \
-# --RTE \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --save \
-# --save-to=RTE-VD-$id 
-# ## Attention Dropout 
-# python train.py \
-# --RTE \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --save \
-# --attention-dropout \
-# --save-to=RTE-AD-$id
 
 # IMDB
 ## Vanilla Dropout 
@@ -52,14 +17,13 @@ python train.py \
 --n-heads=$n_heads \
 --n-layers=$n_layers \
 --batch-size=$batch_size \
---max-epoch=$epochs \
+--max-epoch=1 \
 --max-sent-len=512 \
 --log-every=10 \
---validate-every=10 \
---n-valid=1000 \
---dropout=0.1 \
+--validate-every=50 \
+--n-valid=2000 \
+--dropout=0.3 \
 --lr=3e-5 \
---dset-size=$dset_size \
 --save \
 --save-to=IMDB-VD-$id 
 ## Attention Dropout 
@@ -69,114 +33,57 @@ python train.py \
 --n-heads=$n_heads \
 --n-layers=$n_layers \
 --batch-size=$batch_size \
---max-epoch=$epochs \
+--max-epoch=1 \
 --max-sent-len=512 \
 --log-every=10 \
---validate-every=10 \
---n-valid=1000 \
---dropout=0.1 \
+--validate-every=50 \
+--n-valid=2000 \
+--dropout=0.3 \
 --lr=3e-5 \
---dset-size=$dset_size \
 --save \
 --attention-dropout \
 --save-to=IMDB-AD-$id 
 
 # # COLA
-# ## Vanilla Dropout 
-# python train.py \
-# --COLA \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --dset-size=$dset_size \
-# --save \
-# --save-to=COLA-VD-$id 
-
-# ## Attention Dropout 
-# python train.py \
-# --COLA \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --dset-size=$dset_size \
-# --save \
-# --attention-dropout \
-# --save-to=COLA-AD-$id
-
-# # QNLI
-# ## Vanilla Dropout 
-# python train.py \
-# --QNLI \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --dset-size=$dset_size \
-# --save \
-# --save-to=QNLI-VD-$id 
-# ## Attention Dropout 
-# python train.py \
-# --QNLI \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --dset-size=$dset_size \
-# --save \
-# --attention-dropout \
-# --save-to=QNLI-AD-$id
-
-# # QQP
-# ## Vanilla Dropout 
-# python train.py \
-# --QQP \
-# --hidden-size=$hidden_size \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=10 \
-# --n-valid=1000 \
-# --dropout=0.1 \
-# --lr=3e-5 \
-# --dset-size=$dset_size \
-# --save \
-# --save-to=QQP-VD-$id 
-# ## Attention Dropout 
+## Vanilla Dropout 
 python train.py \
---QQP \
+--COLA \
+--hidden-size=$hidden_size \
+--n-heads=$n_heads \
+--n-layers=$n_layers \
+--batch-size=$batch_size \
+--max-epoch=$epochs \
+--max-sent-len=512 \
+--log-every=20 \
+--validate-every=50 \
+--n-valid=1000 \
+--dropout=0.4 \
+--lr=3e-5 \
+--save \
+--save-to=COLA-VD-$id 
+
+## Attention Dropout 
+python train.py \
+--COLA \
+--hidden-size=$hidden_size \
+--n-heads=$n_heads \
+--n-layers=$n_layers \
+--batch-size=$batch_size \
+--max-epoch=$epochs \
+--max-sent-len=512 \
+--log-every=20 \
+--validate-every=50 \
+--n-valid=1000 \
+--dropout=0.4 \
+--lr=3e-5 \
+--save \
+--attention-dropout \
+--save-to=COLA-AD-$id
+
+# QNLI
+## Vanilla Dropout 
+python train.py \
+--QNLI \
 --hidden-size=$hidden_size \
 --n-heads=$n_heads \
 --n-layers=$n_layers \
@@ -184,11 +91,65 @@ python train.py \
 --max-epoch=$epochs \
 --max-sent-len=512 \
 --log-every=10 \
---validate-every=10 \
---n-valid=1000 \
---dropout=0.1 \
+--validate-every=100 \
+--n-valid=5000 \
+--dropout=0.3 \
 --lr=3e-5 \
---dset-size=$dset_size \
+--dset-size=20000 \
+--save \
+--save-to=QNLI-VD-$id 
+## Attention Dropout 
+python train.py \
+--QNLI \
+--hidden-size=$hidden_size \
+--n-heads=$n_heads \
+--n-layers=$n_layers \
+--batch-size=$batch_size \
+--max-epoch=$epochs \
+--max-sent-len=512 \
+--log-every=10 \
+--validate-every=100 \
+--n-valid=5000 \
+--dropout=0.3 \
+--lr=3e-5 \
+--dset-size=20000 \
+--save \
+--attention-dropout \
+--save-to=QNLI-AD-$id
+
+# QQP
+## Vanilla Dropout 
+python train.py \
+--QQP \
+--hidden-size=$hidden_size \
+--n-heads=$n_heads \
+--n-layers=$n_layers \
+--batch-size=$batch_size \
+--max-epoch=1 \
+--max-sent-len=512 \
+--log-every=10 \
+--validate-every=50 \
+--n-valid=2000 \
+--dropout=0.3 \
+--lr=3e-5 \
+--dset-size=20000 \
+--save \
+--save-to=QQP-VD-$id 
+## Attention Dropout 
+python train.py \
+--QQP \
+--hidden-size=$hidden_size \
+--n-heads=$n_heads \
+--n-layers=$n_layers \
+--batch-size=$batch_size \
+--max-epoch=1 \
+--max-sent-len=512 \
+--log-every=10 \
+--validate-every=50 \
+--n-valid=2000 \
+--dropout=0.3 \
+--lr=3e-5 \
+--dset-size=20000 \
 --save \
 --attention-dropout \
 --save-to=QQP-AD-$id
