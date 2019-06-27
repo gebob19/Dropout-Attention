b3bdec27c251c91c518966835c4da3c309405899
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-27 10:51:54 -0400

    lazy commit

--- train.sh
+++ train.sh
@@ -80,39 +80,37 @@ dset_size="10000"
 # --attention-dropout \
 # --save-to=COLA-AD-$id
 
-# # QNLI
-# ## Vanilla Dropout 
-# python train.py \
-# --QNLI \
-# --hidden-size=768 \
-# --n-heads=$n_heads \
-# --n-layers=$n_layers \
-# --batch-size=$batch_size \
-# --max-epoch=$epochs \
-# --max-sent-len=512 \
-# --log-every=10 \
-# --validate-every=100 \
-# --n-valid=5000 \
-# --dropout=0.5 \
-# --lr=3e-5 \
-# --dset-size=50000 \
-# --save \
-# --save-to=QNLI-VD-$id 
-# Attention Dropout 
+# QNLI
+## Vanilla Dropout 
 python train.py \
 --QNLI \
---hidden-size=192 \
---n-heads=$n_heads \
---n-layers=$n_layers \
+--hidden-size=$hidden_size \
+--n-heads=12 \
+--n-layers=12 \
 --batch-size=$batch_size \
---max-epoch=$epochs \
+--max-epoch=1 \
 --max-sent-len=512 \
 --log-every=10 \
 --validate-every=100 \
 --n-valid=5000 \
---dropout=0.5 \
+--dropout=0.1 \
 --lr=3e-5 \
---dset-size=50000 \
+--save \
+--save-to=QNLI-VD-$id 
+# Attention Dropout 
+python train.py \
+--QNLI \
+--hidden-size=$hidden_size \
+--n-heads=12 \
+--n-layers=12 \
+--batch-size=$batch_size \
+--max-epoch=1 \
+--max-sent-len=512 \
+--log-every=10 \
+--validate-every=100 \
+--n-valid=5000 \
+--dropout=0.1 \
+--lr=3e-5 \
 --save \
 --attention-dropout \
 --save-to=QNLI-AD-$id
