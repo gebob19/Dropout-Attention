42fdd6deed9b175ef0f3c0057449ac0c55a6ea2c
Brennan Gebotys <gebob19.git@gmail.com>
2019-06-26 23:38:52 -0400

    lazy commit

--- train.sh
+++ train.sh
@@ -9,40 +9,40 @@ epochs="2"
 id="JUNE26-UPDATED2-6-12-LAYER"
 dset_size="10000"
 
-# IMDB
-## Vanilla Dropout 
-python train.py \
---IMDB \
---hidden-size=$hidden_size \
---n-heads=12 \
---n-layers=12 \
---batch-size=8 \
---max-epoch=1 \
---max-sent-len=512 \
---log-every=10 \
---validate-every=50 \
---n-valid=2000 \
---dropout=0.3 \
---lr=3e-5 \
---save \
---save-to=IMDB-VD-$id 
-## Attention Dropout 
-python train.py \
---IMDB \
---hidden-size=$hidden_size \
---n-heads=12 \
---n-layers=12 \
---batch-size=8 \
---max-epoch=1 \
---max-sent-len=512 \
---log-every=10 \
---validate-every=50 \
---n-valid=2000 \
---dropout=0.3 \
---lr=3e-5 \
---save \
---attention-dropout \
---save-to=IMDB-AD-$id 
+# # IMDB
+# ## Vanilla Dropout 
+# python train.py \
+# --IMDB \
+# --hidden-size=$hidden_size \
+# --n-heads=12 \
+# --n-layers=12 \
+# --batch-size=8 \
+# --max-epoch=1 \
+# --max-sent-len=512 \
+# --log-every=10 \
+# --validate-every=50 \
+# --n-valid=2000 \
+# --dropout=0.3 \
+# --lr=3e-5 \
+# --save \
+# --save-to=IMDB-VD-$id 
+# ## Attention Dropout 
+# python train.py \
+# --IMDB \
+# --hidden-size=$hidden_size \
+# --n-heads=12 \
+# --n-layers=12 \
+# --batch-size=8 \
+# --max-epoch=1 \
+# --max-sent-len=512 \
+# --log-every=10 \
+# --validate-every=50 \
+# --n-valid=2000 \
+# --dropout=0.3 \
+# --lr=3e-5 \
+# --save \
+# --attention-dropout \
+# --save-to=IMDB-AD-$id 
 
 # # # COLA
 # ## Vanilla Dropout 
@@ -80,24 +80,24 @@ python train.py \
 # --attention-dropout \
 # --save-to=COLA-AD-$id
 
-# QNLI
-## Vanilla Dropout 
-python train.py \
---QNLI \
---hidden-size=768 \
---n-heads=$n_heads \
---n-layers=$n_layers \
---batch-size=$batch_size \
---max-epoch=$epochs \
---max-sent-len=512 \
---log-every=10 \
---validate-every=100 \
---n-valid=5000 \
---dropout=0.5 \
---lr=3e-5 \
---dset-size=50000 \
---save \
---save-to=QNLI-VD-$id 
+# # QNLI
+# ## Vanilla Dropout 
+# python train.py \
+# --QNLI \
+# --hidden-size=768 \
+# --n-heads=$n_heads \
+# --n-layers=$n_layers \
+# --batch-size=$batch_size \
+# --max-epoch=$epochs \
+# --max-sent-len=512 \
+# --log-every=10 \
+# --validate-every=100 \
+# --n-valid=5000 \
+# --dropout=0.5 \
+# --lr=3e-5 \
+# --dset-size=50000 \
+# --save \
+# --save-to=QNLI-VD-$id 
 ## Attention Dropout 
 python train.py \
 --QNLI \
